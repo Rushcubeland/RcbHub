c2754da92ece848c97f0b3f116d5c9d35638fe59
Dylan LANNUZEL <lannuzeld@gmail.com>
2021-11-25 14:09:51 +0100

    Fix Parcours timer

--- src/fr/rushcubeland/rcbhub/parcours/Parcours.java
+++ src/fr/rushcubeland/rcbhub/parcours/Parcours.java
@@ -84,11 +84,14 @@ public class Parcours {
                     PlayerJoin.giveJoinItems(player);
                     AStats aStats = RcbAPI.getInstance().getAccountStats(player);
                     long currentTimer = aStats.getParcoursTimer();
-                    if(timer < currentTimer){
+                    if(currentTimer == null){
+                        aStats.setParcoursTimer(timer);
+                    }
+                    else if(timer < currentTimer){
                         player.sendMessage("§6Félicitations, Vous avez battu votre §crecord personnel !");
                         aStats.setParcoursTimer(timer);
-                        RcbAPI.getInstance().sendAStatsToRedis(aStats);
                     }
+                    RcbAPI.getInstance().sendAStatsToRedis(aStats);
                 }
             }
 
